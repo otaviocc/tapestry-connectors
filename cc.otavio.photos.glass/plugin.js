d1603f863d1a0744d785c84afff48629342f8b1e
Otavio Cordeiro <otaviocc@users.noreply.github.com>
2025-02-16 15:01:01 +0100

    Fix indentation

--- cc.otavio.photos.glass/plugin.js
+++ cc.otavio.photos.glass/plugin.js
@@ -1,5 +1,5 @@
 function identify() {
-  setIdentifier(null)
+	setIdentifier(null)
 }
 
 function verify() {
@@ -11,44 +11,44 @@ function verify() {
 }
 
 function load() {
-  loadAsync()
-    .then(processResults)
-    .catch(processError)
+	loadAsync()
+	.then(processResults)
+	.catch(processError)
 }
 
 async function loadAsync() {
-  const text = await sendRequest("https://glass.photo/" + inputUsername + "/rss");
-  const obj = xmlParse(text);
-  const regex = /<img\s+src="([^"]+)"\s+width="(\d+)"\s+height="(\d+)"/;
-  const results = []
+	const text = await sendRequest("https://glass.photo/" + inputUsername + "/rss");
+	const obj = xmlParse(text);
+	const regex = /<img\s+src="([^"]+)"\s+width="(\d+)"\s+height="(\d+)"/;
+	const results = []
 
-  for (const entry of obj.rss.channel.item) {
-	const date = new Date(entry.pubDate);
-	const match = entry["content:encoded"].match(regex);
+	for (const entry of obj.rss.channel.item) {
+		const date = new Date(entry.pubDate);
+		const match = entry["content:encoded"].match(regex);
 
-	if (!match) continue;
+		if (!match) continue;
 
-	const imageURL = match[1];
-    const width = match[2];
-    const height = match[3];
+		const imageURL = match[1];
+		const width = match[2];
+		const height = match[3];
 
-	const item = Item.createWithUriDate(entry.guid, date);
+		const item = Item.createWithUriDate(entry.guid, date);
 
-	if (inputShowDescriptions == "on") {
-	  item.body = entry.title;
+		if (inputShowDescriptions == "on") {
+			item.body = entry.title;
+		}
+
+		const identity = Identity.createWithName(inputUsername);
+		identity.name = "@" + inputUsername;
+		item.author = identity;
+
+		const attachment = MediaAttachment.createWithUrl(imageURL);
+		attachment.mimeType = "image/jpg";
+		attachment.aspectSize = {width: width, height: height};
+		item.attachments = [attachment];
+
+		results.push(item);
 	}
-	
-	const identity = Identity.createWithName(inputUsername);
-	identity.name = "@" + inputUsername;
-	item.author = identity;
 
-	const attachment = MediaAttachment.createWithUrl(imageURL);
-	attachment.mimeType = "image/jpg";
-	attachment.aspectSize = {width: width, height: height};
-	item.attachments = [attachment];
-	
-	results.push(item);
-  }
-  
-  return results;
+	return results;
 }
--- cc.otavio.pics.some/plugin.js
+++ cc.otavio.pics.some/plugin.js
@@ -1,43 +1,43 @@
 function identify() {
-  setIdentifier(null)
+	setIdentifier(null)
 }
 
 function load() {
-  loadAsync()
-    .then(processResults)
-    .catch(processError)
+	loadAsync()
+	.then(processResults)
+	.catch(processError)
 }
 
 async function loadAsync() {
-  const text = await sendRequest(site + "/rss");
-  const obj = xmlParse(text);
-  const regex = /src="(https:\/\/cdn\.some\.pics\/[^"]+)"(?:.|\n)*?Shared by <a href="https:\/\/[^"]+">@([^<]+)<\/a>/;
-  const results = []
+	const text = await sendRequest(site + "/rss");
+	const obj = xmlParse(text);
+	const regex = /src="(https:\/\/cdn\.some\.pics\/[^"]+)"(?:.|\n)*?Shared by <a href="https:\/\/[^"]+">@([^<]+)<\/a>/;
+	const results = []
 
-  for (const entry of obj.rss.channel.item) {
-	const date = new Date(entry.pubDate);
-	const match = entry.description.match(regex);
+	for (const entry of obj.rss.channel.item) {
+		const date = new Date(entry.pubDate);
+		const match = entry.description.match(regex);
 
-	if (!match) continue;
+		if (!match) continue;
 
-	const imageURL = match[1];
-	const author = match[2];
-	const item = Item.createWithUriDate(entry.guid, date);
-	item.body = entry.title;
+		const imageURL = match[1];
+		const author = match[2];
+		const item = Item.createWithUriDate(entry.guid, date);
+		item.body = entry.title;
 
-	const identity = Identity.createWithName(author);
-	identity.name = "@" + author;
-	identity.avatar = "https://profiles.cache.lol/" + author + "/picture";
-	item.author = identity;
+		const identity = Identity.createWithName(author);
+		identity.name = "@" + author;
+		identity.avatar = "https://profiles.cache.lol/" + author + "/picture";
+		item.author = identity;
 
-	const attachment = MediaAttachment.createWithUrl(imageURL);
-	attachment.mimeType = "image";
-	attachment.aspectFit = {width: 400, height: 300};
-	attachment.focalPoint = {x: 0, y: 0};
-	item.attachments = [attachment];
-	
-	results.push(item);
-  }
-  
-  return results;
+		const attachment = MediaAttachment.createWithUrl(imageURL);
+		attachment.mimeType = "image";
+		attachment.aspectFit = {width: 400, height: 300};
+		attachment.focalPoint = {x: 0, y: 0};
+		item.attachments = [attachment];
+
+		results.push(item);
+	}
+
+	return results;
 }
